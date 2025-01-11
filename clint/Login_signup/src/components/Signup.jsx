3419a3ff5320c334756da572b35800303e44232b
Subham kr Sah <subhamgithub223@gmail.com>
2025-01-11 18:17:49 +0530

    Update Signup.jsx

--- clint/Login_signup/src/components/Signup.jsx
+++ clint/Login_signup/src/components/Signup.jsx
@@ -31,7 +31,7 @@ function Signup() {
         //     return handelError('Name , Email, Password are required!')
         // }
         try {
-            const url = "http://localhost:8001/auth/signup";
+            const url = "https://login-signup-mern-stack-omega.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
